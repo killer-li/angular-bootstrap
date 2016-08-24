47e12ffbc131a728a8adfe6367c5d9e7bf7b374b
lishichao1002 <lishichao1002@163.com>
2016-08-24 13:32:14 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+var webpack = require('webpack');
+var ngAnnotatePlugin = require('ng-annotate-webpack-plugin');
+
+module.exports = {
+    //页面入口文件配置
+    entry: {
+        index: './src/index.js'
+    },
+    //入口文件输出配置
+    output: {
+        path: 'src/',
+        filename: '[name].bundle.js'
+    },
+    module: {
+        //加载器配置
+        loaders: [{
+            test: /\.css$/,
+            loader: 'style-loader!css-loader'
+        }, {
+            test: /\.(png|jpg|gif|woff|woff2|eot|ttf)$/,
+            loader: 'url-loader?limit=102040'
+        }, {
+            test: /\.html$/,
+            loader: 'html',
+        }],
+        htmlLoader: {
+            ignoreCustomFragments: [/\{\{.*?}}/]
+        }
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        /* 处理angular的依赖注入 */
+        // new ngAnnotatePlugin({
+        //     add: true
+        // }),
+        /* 处理代码压缩 */
+        // new webpack.optimize.UglifyJsPlugin({
+        //     compress: {
+        //         warnings: false
+        //     },
+        //     output: {
+        //         comments: false
+        //     }
+        // })
+    ]
+};
