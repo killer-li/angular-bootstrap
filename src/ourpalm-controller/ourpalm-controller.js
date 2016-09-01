306de9f55de59caf9b74919530a1795bf1e63026
lishichao1002 <lishichao1002@163.com>
2016-09-01 18:39:52 +0800

    modify topic

--- src/ourpalm-controller/ourpalm-controller.js
+++ src/ourpalm-controller/ourpalm-controller.js
@@ -6,12 +6,22 @@
         /**
          * 最热帖子 最新帖子
          */
-        .controller('TopicController', function (Service) {
+        .controller('TopicController', function (Service, $uibModal) {
             var vm = this;
 
             Service.loadTopic().success(function (result) {
                 vm.topic = result.data;
             });
+
+            vm.openSettingColumnModal = function () {
+                var modalInstance = $uibModal.open({
+                    templateUrl: 'setting-column-tpl'
+                });
+
+                modalInstance.result.then(function (selectedItem) {
+
+                });
+            };
         })
 
         /**
